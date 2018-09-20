81ef4baf471d9406bc4333ebb70ec4f3c5c97de6
DanishAfzal <d.afzal@hotmail.com>
2018-09-20 22:05:57 +0200

    Console Start Screen

--- Sokoban/Sokoban/OutputView.cs
+++ Sokoban/Sokoban/OutputView.cs
@@ -8,5 +8,42 @@ namespace Sokoban
     public class OutputView
     {
 
+        public void startScreen()
+        {
+            Console.WriteLine("______________________________________________________");
+            Console.WriteLine("|                              |                     |");
+            Console.WriteLine("|Welkom bij Sokoban!           |                     |");
+            Console.WriteLine("|                              |                     |");
+            Console.WriteLine("|                              |                     |");
+            Console.WriteLine("|Betekenis van de symbolen" + 
+                "     |   doel van het spel:|");
+
+            Console.WriteLine("|Spatie : outerspace" + 
+                "           |    Duw met de truck |");
+            Console.WriteLine("|█ : muur" +
+                "                      |    de krat(ten)     |");
+            Console.WriteLine(". : vloer" + 
+                "                      |   Naar de bestemming|");
+
+            Console.WriteLine("|O : krat                      |                     |");
+            Console.WriteLine("|0 : krat of bestemming        |                     |");
+            Console.WriteLine("|x : bestemming                |                     |");
+            Console.WriteLine("|@ : truck                     |                     |");
+            Console.WriteLine("______________________________________________________");
+
+
+            Console.WriteLine("> Kies een doolhof (1 -4), s = stop");
+            Console.WriteLine("Enjoy!");
+            string s = Console.ReadLine();
+
+            if (s.Equals("s")) {
+                Environment.Exit(0);
+            }else
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+
     }
 }
