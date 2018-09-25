b20e2d3d13d0da6ca1024b5fdd411d427bd4221a
DanishAfzal <d.afzal@hotmail.com>
2018-09-25 12:51:34 +0200

    Swap swap swap
    lets get em :fire:

--- Sokoban/Sokoban/ViewModel/GameLogic.cs
+++ Sokoban/Sokoban/ViewModel/GameLogic.cs
@@ -1,9 +1,33 @@
-﻿namespace Sokoban
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Sokoban
 {
     public class GameLogic
     {
         public GameLogic()
         {
+
+            
+        }
+
+        public void swapToRight()
+        {
+
+        }
+
+        public void swapToLeft()
+        {
+
+        }
+
+        public void swaptoTop()
+        {
+
+        }
+
+        public void swapToBottom()
+        {
+
         }
     }
 }
