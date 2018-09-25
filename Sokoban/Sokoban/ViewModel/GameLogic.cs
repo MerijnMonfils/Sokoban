6a769fe2c12b3e9e8cb1161405b5ee5392f57feb
Merijn Monfils <merijn.monfils@gmail.com>
2018-09-25 13:02:12 +0200

    gamelogic fix pls

--- Sokoban/Sokoban/ViewModel/GameLogic.cs
+++ Sokoban/Sokoban/ViewModel/GameLogic.cs
@@ -1,33 +1,9 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace Sokoban
+﻿namespace Sokoban
 {
     public class GameLogic
     {
         public GameLogic()
         {
-
-            
-        }
-
-        public void swapToRight()
-        {
-
-        }
-
-        public void swapToLeft()
-        {
-
-        }
-
-        public void swaptoTop()
-        {
-
-        }
-
-        public void swapToBottom()
-        {
-
         }
     }
 }
